63d8cfa435be73d1abf3cd7c1e7f03e411e034f0
manish <maneeshbalami@gmail.com>
2024-08-11 18:22:02 +0545

    added viewsets and routers for posts

---
+++ backend/posts/serializers.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Post
+    fields = ('id', 'author', 'title', 'content', 'created_at',)
+    
