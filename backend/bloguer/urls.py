9fee9d0bd6044b37d2d628464515f7b42b027c60
manish <maneeshbalami@gmail.com>
2024-08-13 13:12:46 +0545

    corrected project urls to use jwt endpoints

--- backend/bloguer/urls.py
+++ backend/bloguer/urls.py
@@ -20,10 +20,10 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('posts.urls')),
+    path('api/post/', include('posts.urls')),
     # path('api-auth/', include('rest_framework.urls')),
     path('api/auth/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.jwt')),
+    # path('api/auth/', include('djoser.urls.jwt')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
