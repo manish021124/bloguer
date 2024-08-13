21cd6627ae0cc4f34abde9e2de8a6292078d2d6b
manish <maneeshbalami@gmail.com>
2024-08-13 16:38:54 +0545

    added custom permission so that only author could manipulate their posts

--- backend/bloguer/settings.py
+++ backend/bloguer/settings.py
@@ -142,7 +142,7 @@ CORS_ORIGIN_WHITELIST = [
 
 REST_FRAMEWORK = {
   'DEFAULT_PERMISSION_CLASSES': [
-    'rest_framework.permissions.AllowAny',
+    'rest_framework.permissions.IsAuthenticated',
   ],
   'DEFAULT_AUTHENTICATION_CLASSES': [
     'rest_framework_simplejwt.authentication.JWTAuthentication',
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
 class PostViewSet(viewsets.ModelViewSet):
+  permission_classes = (IsAuthorOrReadOnly,)
   queryset = Post.objects.all()
   serializer_class = PostSerializer
