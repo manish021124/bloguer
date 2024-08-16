6cf6cce3d5ca37434ee4a77cb33240860abaf561
manish <maneeshbalami@gmail.com>
2024-08-16 10:15:55 +0545

    added navbar

--- frontend/bloguer/src/app/page.tsx
+++ frontend/bloguer/src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1 className="p-3">hello world!</h1>
+    <h1 className="p-3">!</h1>
   );
 }
---
+++ frontend/bloguer/src/app/ui/nav-links.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+export function NavLinks() {
+  const pathname = usePathname()
+
+  return (
+    <nav className='uppercase h-10 px-5 flex items-center space-x-4 text-sm text-gray-400 border-b border-slate-700 font-semibold'>
+      <Link className={`link hover:text-white ${pathname === '/' ? 'text-white' : 'text-gray-400'}`} href="/">
+        Home
+      </Link>
+      <Link className={`link hover:text-white ${pathname === '/about' ? 'text-white' : 'text-gray-400'}`} href="/about">
+        About
+      </Link>
+    </nav>
+  )
+}
