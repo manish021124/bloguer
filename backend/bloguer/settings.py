b9540c884b517772012b3d2fa9b5e1beda786b6a
manish <maneeshbalami@gmail.com>
2025-11-24 17:11:54 +0545

    updates

--- backend/bloguer/settings.py
+++ backend/bloguer/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,7 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
---
+++ backend/entrypoint.sh
@@ -0,0 +1,10 @@
+# Apply database migrations
+echo "Applying database migrations..."
+python manage.py migrate --noinput
+
+# Collect static files (optional, if using static files)
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+# Execute the CMD from the Dockerfile (starts Gunicorn server)
+exec "$@"
